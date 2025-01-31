119cfa9547a0feee0846711ba723fe82d38517bf
tanmayxx12 <troy12rollsza@gmail.com>
2025-02-01 01:40:44 +0530

    Initial Commit

---
+++ iNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iNote
+//
+//  Created by Tanmay . on 01/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iNote/iNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNoteApp.swift
+//  iNote
+//
+//  Created by Tanmay . on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct iNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
